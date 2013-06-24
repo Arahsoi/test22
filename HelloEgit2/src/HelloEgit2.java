911ff7e1b55441af11c17a58037337becefc6355
Jaeyoon Jeong <sbjeong222@gmail.com>
2013-06-24 19:28:23 +0900

    Second Egit Project

---
+++ HelloEgit2/src/HelloEgit2.java
@@ -0,0 +1,12 @@
+
+public class HelloEgit2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello Egit2");
+	}
+
+}
